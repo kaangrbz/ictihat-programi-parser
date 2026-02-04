c6b3b70d595e9426722f055d8367a299ab8ce4e1
Kaan Gürbüz <abdikaangrbz@gmail.com>
2026-02-04 23:58:35 +0300

    update cors

--- server.js
+++ server.js
@@ -47,11 +47,12 @@ loadDictionary();
 const app = express();
 app.use(express.json({ limit: '2mb' }));
 
-// CORS: frontend farklı porttan (Vite dev) veya farklı origin'den istek atabilsin
+// CORS: tüm origin'lere izin (herkese açık)
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+  res.set('Access-Control-Max-Age', '86400');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
